61797bf43bc5cc8177038a03541f2e928f200c3d
Pauraic <pauraic.neely@ucdconnect.ie>
2025-03-23 17:12:32 +0000

    initializing

---
+++ actor_director_comboes.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import ast
+
+credits = pd.read_csv("tmdb_5000_credits.csv")
+box_office = pd.read_csv("boxoffice_data_2024.csv")
+
+credits.columns = credits.columns.str.lower()
+box_office.columns = box_office.columns.str.lower()
+
+def parse_rwo(row):
+    cast = ast.literal_eval(row["cast"])
+    crew = ast.literal_eval(row["crew"])
+
+#get dirs and actors
+    directors = [person["name"] for person in crew if person["job"] == "Director"]
+    actors = [person["name"] for person in cast]
+
+    return [(director,actor,row["title"]) for director in directors for actor in actors]
+
+pairs = credits.apply(parse_rwo,axis=1).explode()
+
+df = pd.DataFrame(pairs.tolist(),columns=["director","actor","title"])
+#works
+
+merged = pd.merge(df,box_office, on="title")
+
+print(merged)
+
+
