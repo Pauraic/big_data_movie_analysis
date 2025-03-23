b35f1c86d51341da5691fa5ba585b92056081d77
Pauraic <pauraic.neely@ucdconnect.ie>
2025-03-23 17:27:58 +0000

    analysis on highest net revenue on actor director comboes

--- actor_director_comboes.py
+++ actor_director_comboes.py
@@ -6,6 +6,8 @@ box_office = pd.read_csv("boxoffice_data_2024.csv")
 
 credits.columns = credits.columns.str.lower()
 box_office.columns = box_office.columns.str.lower()
+box_office["gross"] = box_office["gross"].replace({r'\$':'',',':''}, regex=True)
+box_office["gross"] = pd.to_numeric(box_office["gross"],errors="coerce")
 
 def parse_rwo(row):
     cast = ast.literal_eval(row["cast"])
@@ -24,6 +26,12 @@ df = pd.DataFrame(pairs.tolist(),columns=["director","actor","title"])
 
 merged = pd.merge(df,box_office, on="title")
 
-print(merged)
+
+
+#grouped = merged.groupby(["director","actor"]).agg(avg_revenue=("gross","mean")).reset_index().sort_values(by="avg_revenue",ascending=False)
+grouped= merged.groupby(["director","actor"]).agg(net_revenue=("gross","sum")).reset_index().sort_values(by="net_revenue",ascending=False)
+
+
+print(grouped)
 
 
